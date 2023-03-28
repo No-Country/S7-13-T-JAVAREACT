86f15b3d4c73b57ce8bdadb9c43ef23945502ab7
AXG25 <axiever.guejia@gmail.com>
2023-03-27 20:41:01 -0500

    feat(server/model): Create Usuario Entity

---
+++ server/src/main/java/model/Usuario.java
@@ -0,0 +1,22 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nombre;
+
+    private String email;
+
+    // Getters y Setters
+}
+
